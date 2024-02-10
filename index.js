fdee5b821fb5425103a2df65908c95fe8ca1f56c
Jamie Lee Rogers <156160941+JLRcodes@users.noreply.github.com>
2024-02-10 17:06:42 +1100

    Add two new functions.
    checkWinner which helps decide who has won the game.
    
    playRound which gives us the result and the winner or loser message.

--- index.js
+++ index.js
@@ -2,9 +2,45 @@ console.log("Hi");
 
 const options = ["rock", "paper", "scissors"];
 
+//Generates random choice of rock, paper, scissors
 function getComputerChoice() {
     const choice = options[Math.floor(Math.random() * options.length)];
+    console.log("Computer chose: " + choice)
     return choice;
 }
 
-getComputerChoice();
+// Checks who has won the game. Player or Computer
+function checkWinner(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection){
+        return "Tie!";
+    }
+    else if(
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") || 
+        (playerSelection === "paper" && computerSelection === "rock")
+    ){
+        return "Player";
+    }
+    else {
+        return "Computer";
+    }
+}
+
+
+// Plays a round and gives us a result 
+function playRound(playerSelection, computerSelection) {
+    const result = checkWinner(playerSelection, computerSelection);
+    if(result === "Tie") {
+        return "It's a Tie!";
+    }
+    else if(result === "Player"){
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else{
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
